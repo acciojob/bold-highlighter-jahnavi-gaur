2f6cf82a6fea568f3e51b22312eb63d5bb7f8bf9
jahnavi-gaur <121301783+jahnavi-gaur@users.noreply.github.com>
2026-02-19 08:06:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+let strongs = document.querySelectorAll('strong')
+
 function highlight() {
     //Write your code here
-
-
+	strongs.forEach((strong)=>{
+		strong.style.color = 'rgb(0, 128, 0)'
+	})
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	strongs.forEach((strong)=>{
+		strong.style.color = 'rgb(0, 0, 0)'
+	})
 }
